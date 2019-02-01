796cf7a59f56c2f33de703e5efb25a279e407c6b
Dominik Piekarski <piekarski_d@o2.pl>
2019-02-01 19:32:31 +0100

    some cleanup

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const search = require('./search');
 
 const baseLink = 'https://niebezpiecznik.pl/page/'; // base of links to iterate through
 const numPages = 10; // number of pages
+const output = 'console'; // fileName for output to file or "console" to simply console.log
+const bodySelector = 'div.post p'; // without body, we will have output wihout it
+const postSelector = 'div.post';
+const dateSelector = 'article > div.inner > div > div > div > a > span > small';
+const titleSelector = 'div.post > div.title > h2 > a';
+const linkSelector = 'div.post > div.title > h2 > a';
 
 const links = [];
 for (let i = 1; i < numPages; i++) {
@@ -10,12 +16,11 @@ for (let i = 1; i < numPages; i++) {
 
 const query = {
   links,
-  postSelector: 'div.post',
-  dateSelector: 'article > div.inner > div > div > div > a > span > small',
-  titleSelector: 'div.post > div.title > h2 > a',
-  linkSelector: 'div.post > div.title > h2 > a',
-  bodySelector: 'div.post p' // without body, we will have output wihout
+  postSelector,
+  dateSelector,
+  titleSelector,
+  linkSelector,
+  bodySelector
 };
-const output = 'console'; // fileName for output to file or "console" to simply console.log
 
 search(query, output);
